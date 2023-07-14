da95d9baa4a5fdd5f93d9e48e71758fab9c57577
coderswat <coderswat@gmail.com>
2023-07-15 01:18:18 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Icon React
+
+**Icon React** is a acode plugin to change the icon of `.jsx` and `.tsx` files, from js icon to react icon 
+
+## installation 
+
+- Install **Icon React** Plugin from `Acode App > Settings > Plugins > And search here for Icon React` and install it
+
+## Screenshot
+
+![Example](https://github.com/coswat/acode-code-commenter/assets/97345827/66e461b8-7c4e-4521-893b-8aa374a25e1c)
+
+### Open source:
+Want to see the code? click [here](https://github.com/coswat/acode-icon-react) 
+and don't forget the little star!
+
+### Report Bugs:
+Found bugs? know how to fix report [here!](https://github.com/coswat/acode-icon-react/issues)
+
+> 👾 Thanks for using our plugin!
---
+++ src/main.js
@@ -0,0 +1,51 @@
+import plugin from "../plugin.json";
+import icon from './react.svg';
+
+class IconReact {
+  async init() {
+    this.style = (
+      <style
+        textContent={`.file_type_jsx::before{
+          display: inline-block;
+          content: '';
+          background-image: url(${icon});
+          background-size: contain;
+          background-repeat: no-repeat;
+          height: 1em;
+          width: 1em;
+        }
+        .file_type_tsx::before {
+          display: inline-block;
+          content: '';
+          background-image: url(${icon});
+          background-size: contain;
+          background-repeat: no-repeat;
+          height: 1em;
+          width: 1em;        
+        }`}
+      ></style>
+    );
+    document.head.append(this.style);
+  }
+
+  async destroy() {
+    //
+  }
+}
+
+if (window.acode) {
+  const acodePlugin = new IconReact();
+  acode.setPluginInit(
+    plugin.id,
+    (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+      if (!baseUrl.endsWith("/")) {
+        baseUrl += "/";
+      }
+      acodePlugin.baseUrl = baseUrl;
+      acodePlugin.init($page, cacheFile, cacheFileUrl);
+    }
+  );
+  acode.setPluginUnmount(plugin.id, () => {
+    acodePlugin.destroy();
+  });
+}
